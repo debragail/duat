7be213d8e4101e0375266cd76ab7f4ca0774a08b
O <git@27c1ub.com>
2018-11-12 22:57:32 -0800

    postman update

--- app/index.js
+++ app/index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
-const bc = Blockchain();
+const bc = new Blockchain();
+
+
+app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
 
-app.listen(HTTP_PORT), () => console.log(`Listening no port ${HTTP_PORT}`));
+app.post('/mine', (req, res) => {
+    const block = bc.addBlock(req.body.data)
+    console.log(`New Block added: ${block.toString()}`);
+
+    res.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`Listening no port ${HTTP_PORT}`));
